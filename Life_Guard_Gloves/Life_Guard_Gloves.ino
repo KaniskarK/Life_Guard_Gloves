b3e7fcf282c453401f683656792e750df55bbaa4
KANISKAR K <76870433+KaniskarK@users.noreply.github.com>
2023-01-26 21:28:54 +0530

    Update Life_Guard_Gloves.ino

--- Life_Guard_Gloves/Life_Guard_Gloves.ino
+++ Life_Guard_Gloves/Life_Guard_Gloves.ino
@@ -1,33 +1,28 @@
 #include <ESP8266WiFi.h>    //esp8266 library
 #include <FirebaseArduino.h>     //firebase library
+#include <Wire.h>
+#include <Adafruit_ADS1015.h>
+Adafruit_ADS1115 ads(0x48);
 #include <OneWire.h>
 #include <DallasTemperature.h>
-#include <PulseSensorPlayground.h>     // Includes the PulseSensorPlayground Library
-#define FIREBASE_HOST "healthcare-7c153.firebaseio.com"  // the project name address from firebase id
-#define FIREBASE_AUTH "qxAJyMdw8DdKt2RIvaBZdlyRBa7GcwbX1pfiv6Cm"  // the secret key generated from firebase
-#define WIFI_SSID "XXXXXXXXXXXX"                  // wifi name 
-#define WIFI_PASSWORD "XXXXXXXXX"                 //password of wifi 
-const int PulseWire = 0;  
+const int SensorDataPin = 14;
+OneWire oneWire(SensorDataPin);
+DallasTemperature sensors(&oneWire);
+#define FIREBASE_HOST ""//the project name address from firebase id
+#define FIREBASE_AUTH ""//the secret key generated from firebase
+#define WIFI_SSID "R&D LABS"                  // wifi name
+#define WIFI_PASSWORD ""                 //password of wifi
+const int PulseWire = 0;
 float temp;
-float Fahrenheit=0;
-int Threshold = 550;    
-int sensorvalue=A0;
-const int flexPin = A1; 
-const int flexPin = A2; 
-const int flexPin = A3 
-const int flexPin = A4; 
-int motorPin = 3;
-int value=0;
-motorpin==value;
-                                                    
+float Fahrenheit = 0;
+int Threshold = 550;
+int value = 0;
 void setup() {
   Serial.begin(9600);
   delay(1000);
-  pinMode(motorPin, OUTPUT );
-  pulseSensor.analogInput(PulseWire);
-  pulseSensor.setThreshold(Threshold);   
-  DS18B20.begin();                
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);    
+  sensors.begin();
+  ads.begin();
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("Connecting to ");
   Serial.print(WIFI_SSID);
   while (WiFi.status() != WL_CONNECTED) {
@@ -40,413 +35,48 @@ void setup() {
   Serial.print("IP Address is : ");
   Serial.println(WiFi.localIP());                  //print local IP address
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);     // connect to firebase
-  if (pulseSensor.begin()) {
-    Serial.println("PulseSensor object created!");
-  }                         //Start reading dht sensor
 }
-void gas_level()
+void loop()
 {
-   int sensorValue = analogRead(A0);
-  // print out the value you read:
-  Serial.println(sensorValue);
-  delay(200);
-}
-void flex_sensor()
-{
-  int flexValue1;
-  int flexValue2;
-  int flexValue3;
-  int flexValue4;
-  flexValue = analogRead(flexPin);
-  flexValue = analogRead(flexPin);
-  flexValue = analogRead(flexPin);
-  flexValue = analogRead(flexPin);
-  Serial.print("sensor: ");
-  Serial.println(flexValue1);
-  Serial.print("sensor: ");
-  Serial.println(flexValue2);
-  Serial.print("sensor: ");
-  Serial.println(flexValue3);
-  Serial.print("sensor: ");
-  Serial.println(flexValue4);
-  }
-void loop() { 
-if(motorPin==HIGH)
-{
-    digitalWrite(motorPin, HIGH);
-    Serial.println("vibration detected");
-    Serial.println(value);
-    delay(1000);
-}
-else{
-    digitalWrite(motorPin, LOW);
-    Serial.println("vibration not detected");
-    Serial.println(value);
-    delay(1000);
-}
-  DS18B20.requestTemperatures(); 
-  temp = DS18B20.getTempCByIndex(0); // Celcius
-  Fahrenheit = DS18B20.toFahrenheit(temp); // Fahrenheit
+  int16_t adc0, adc1, adc2, adc3;
+  adc0 = ads.readADC_SingleEnded(0);
+  adc1 = ads.readADC_SingleEnded(1);
+  adc2 = ads.readADC_SingleEnded(2);
+  adc3 = ads.readADC_SingleEnded(3);
+  String vib_data = String(adc0);
+  String myBPM = String(adc1);
+  String sensorValue =String(adc2);
+  String flexValue = String(adc3);
+  sensors.requestTemperatures();
+  float temperature_Celsius = sensors.getTempCByIndex(0);
+  float temperature_Fahrenheit = sensors.getTempFByIndex(0);
+  Serial.print("Temperature: ");
+  Serial.print(temperature_Celsius);
+  Serial.println(" ºC");
+  Serial.print("Temperature: ");
+  Serial.print(temperature_Fahrenheit);
+  Serial.println(" ºF");
+  Serial.println("");
+  Serial.println("temp:");
   Serial.println(temp);
+  String t = String(temp);
+  Serial.println("Fahrenheit:");
   Serial.println(Fahrenheit);
-  delay(4000);
-int myBPM = pulseSensor.getBeatsPerMinute();      // Calculates BPM
-
-  if (pulseSensor.sawStartOfBeat()) {               // Constantly test to see if a beat happened
-    Serial.println("♥  A HeartBeat Happened ! "); // If true, print a message
-    Serial.print("BPM: ");
-    Serial.println(myBPM);                        // Print the BPM value
-    }
-delay(20);
-gas_level()
-flex_sensor()
-  Firebase.pushString("/Healthcare/Temperature", temp);         //setup path and send readings
-  Firebase.pushString("/Healthcare/Temperature", Fahrenheit);        //setup path and send readings
-  Firebase.pushString("/Healthcare/Heartbeat", myBPM);   
-  Firebase.pushString("/Healthcare/Gasvalue", sensorValue);  
-  Firebase.pushString("/Healthcare/Flexrange1", flexValue1);    
-  Firebase.pushString("/Healthcare/Flexrange2", flexValue2);    
-  Firebase.pushString("/Healthcare/Flexrange3", flexValue3);    
-  Firebase.pushString("/Healthcare/Flexrange4", flexValue4);    
-  Firebase.pushString("/Healthcare/vibration", value); 
-   
-}
-#include <ESP8266WiFi.h>    //esp8266 library
-#include <FirebaseArduino.h>     //firebase library
-#include <OneWire.h>
-#include <DallasTemperature.h>
-#include <PulseSensorPlayground.h>     // Includes the PulseSensorPlayground Library
-#define FIREBASE_HOST "healthcare-7c153.firebaseio.com"  // the project name address from firebase id
-#define FIREBASE_AUTH "qxAJyMdw8DdKt2RIvaBZdlyRBa7GcwbX1pfiv6Cm"  // the secret key generated from firebase
-#define WIFI_SSID "XXXXXXXXXXXX"                  // wifi name 
-#define WIFI_PASSWORD "XXXXXXXXX"                 //password of wifi 
-const int PulseWire = 0;  
-float temp;
-float Fahrenheit=0;
-int Threshold = 550;    
-int sensorvalue=A0;
-const int flexPin = A1; 
-const int flexPin = A2; 
-const int flexPin = A3 
-const int flexPin = A4; 
-int motorPin = 3;
-int value=0;
-motorpin==value;
-                                                    
-void setup() {
-  Serial.begin(9600);
+  String Fah = String(Fahrenheit);
+  Serial.println("vib_data:");
+  Serial.println(vib_data);
+  Serial.println("myBPM:");
+  Serial.println(myBPM);
+  Serial.println("sensorValue:");
+  Serial.println(sensorValue);
+  Serial.println("flexValue:");
+  Serial.println(flexValue);
   delay(1000);
-  pinMode(motorPin, OUTPUT );
-  pulseSensor.analogInput(PulseWire);
-  pulseSensor.setThreshold(Threshold);   
-  DS18B20.begin();                
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);    
-  Serial.print("Connecting to ");
-  Serial.print(WIFI_SSID);
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(500);
-  }
-  Serial.println();
-  Serial.print("Connected to ");
-  Serial.println(WIFI_SSID);
-  Serial.print("IP Address is : ");
-  Serial.println(WiFi.localIP());                  //print local IP address
-  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);     // connect to firebase
-  if (pulseSensor.begin()) {
-    Serial.println("PulseSensor object created!");
-  }                         //Start reading dht sensor
-}
-void gas_level()
-{
-   int sensorValue = analogRead(A0);
-  // print out the value you read:
-  Serial.println(sensorValue);
-  delay(200);
-}
-void flex_sensor()
-{
-  int flexValue1;
-  int flexValue2;
-  int flexValue3;
-  int flexValue4;
-  flexValue = analogRead(flexPin);
-  flexValue = analogRead(flexPin);
-  flexValue = analogRead(flexPin);
-  flexValue = analogRead(flexPin);
-  Serial.print("sensor: ");
-  Serial.println(flexValue1);
-  Serial.print("sensor: ");
-  Serial.println(flexValue2);
-  Serial.print("sensor: ");
-  Serial.println(flexValue3);
-  Serial.print("sensor: ");
-  Serial.println(flexValue4);
-  }
-void loop() { 
-if(motorPin==HIGH)
-{
-    digitalWrite(motorPin, HIGH);
-    Serial.println("vibration detected");
-    Serial.println(value);
-    delay(1000);
-}
-else{
-    digitalWrite(motorPin, LOW);
-    Serial.println("vibration not detected");
-    Serial.println(value);
-    delay(1000);
-}
-  DS18B20.requestTemperatures(); 
-  temp = DS18B20.getTempCByIndex(0); // Celcius
-  Fahrenheit = DS18B20.toFahrenheit(temp); // Fahrenheit
-  Serial.println(temp);
-  Serial.println(Fahrenheit);
-  delay(4000);
-int myBPM = pulseSensor.getBeatsPerMinute();      // Calculates BPM
-
-  if (pulseSensor.sawStartOfBeat()) {               // Constantly test to see if a beat happened
-    Serial.println("♥  A HeartBeat Happened ! "); // If true, print a message
-    Serial.print("BPM: ");
-    Serial.println(myBPM);                        // Print the BPM value
-    }
-delay(20);
-gas_level()
-flex_sensor()
-  Firebase.pushString("/Healthcare/Temperature", temp);         //setup path and send readings
-  Firebase.pushString("/Healthcare/Temperature", Fahrenheit);        //setup path and send readings
-  Firebase.pushString("/Healthcare/Heartbeat", myBPM);   
-  Firebase.pushString("/Healthcare/Gasvalue", sensorValue);  
-  Firebase.pushString("/Healthcare/Flexrange1", flexValue1);    
-  Firebase.pushString("/Healthcare/Flexrange2", flexValue2);    
-  Firebase.pushString("/Healthcare/Flexrange3", flexValue3);    
-  Firebase.pushString("/Healthcare/Flexrange4", flexValue4);    
-  Firebase.pushString("/Healthcare/vibration", value); 
-   
-}
-#include <ESP8266WiFi.h>    //esp8266 library
-#include <FirebaseArduino.h>     //firebase library
-#include <OneWire.h>
-#include <DallasTemperature.h>
-#include <PulseSensorPlayground.h>     // Includes the PulseSensorPlayground Library
-#define FIREBASE_HOST "healthcare-7c153.firebaseio.com"  // the project name address from firebase id
-#define FIREBASE_AUTH "qxAJyMdw8DdKt2RIvaBZdlyRBa7GcwbX1pfiv6Cm"  // the secret key generated from firebase
-#define WIFI_SSID "XXXXXXXXXXXX"                  // wifi name 
-#define WIFI_PASSWORD "XXXXXXXXX"                 //password of wifi 
-const int PulseWire = 0;  
-float temp;
-float Fahrenheit=0;
-int Threshold = 550;    
-int sensorvalue=A0;
-const int flexPin = A1; 
-const int flexPin = A2; 
-const int flexPin = A3 
-const int flexPin = A4; 
-int motorPin = 3;
-int value=0;
-motorpin==value;
-                                                    
-void setup() {
-  Serial.begin(9600);
-  delay(1000);
-  pinMode(motorPin, OUTPUT );
-  pulseSensor.analogInput(PulseWire);
-  pulseSensor.setThreshold(Threshold);   
-  DS18B20.begin();                
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);    
-  Serial.print("Connecting to ");
-  Serial.print(WIFI_SSID);
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(500);
-  }
-  Serial.println();
-  Serial.print("Connected to ");
-  Serial.println(WIFI_SSID);
-  Serial.print("IP Address is : ");
-  Serial.println(WiFi.localIP());                  //print local IP address
-  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);     // connect to firebase
-  if (pulseSensor.begin()) {
-    Serial.println("PulseSensor object created!");
-  }                         //Start reading dht sensor
-}
-void gas_level()
-{
-   int sensorValue = analogRead(A0);
-  // print out the value you read:
-  Serial.println(sensorValue);
-  delay(200);
-}
-void flex_sensor()
-{
-  int flexValue1;
-  int flexValue2;
-  int flexValue3;
-  int flexValue4;
-  flexValue = analogRead(flexPin);
-  flexValue = analogRead(flexPin);
-  flexValue = analogRead(flexPin);
-  flexValue = analogRead(flexPin);
-  Serial.print("sensor: ");
-  Serial.println(flexValue1);
-  Serial.print("sensor: ");
-  Serial.println(flexValue2);
-  Serial.print("sensor: ");
-  Serial.println(flexValue3);
-  Serial.print("sensor: ");
-  Serial.println(flexValue4);
-  }
-void loop() { 
-if(motorPin==HIGH)
-{
-    digitalWrite(motorPin, HIGH);
-    Serial.println("vibration detected");
-    Serial.println(value);
-    delay(1000);
-}
-else{
-    digitalWrite(motorPin, LOW);
-    Serial.println("vibration not detected");
-    Serial.println(value);
-    delay(1000);
-}
-  DS18B20.requestTemperatures(); 
-  temp = DS18B20.getTempCByIndex(0); // Celcius
-  Fahrenheit = DS18B20.toFahrenheit(temp); // Fahrenheit
-  Serial.println(temp);
-  Serial.println(Fahrenheit);
-  delay(4000);
-int myBPM = pulseSensor.getBeatsPerMinute();      // Calculates BPM
-
-  if (pulseSensor.sawStartOfBeat()) {               // Constantly test to see if a beat happened
-    Serial.println("♥  A HeartBeat Happened ! "); // If true, print a message
-    Serial.print("BPM: ");
-    Serial.println(myBPM);                        // Print the BPM value
-    }
-delay(20);
-gas_level()
-flex_sensor()
-  Firebase.pushString("/Healthcare/Temperature", temp);         //setup path and send readings
-  Firebase.pushString("/Healthcare/Temperature", Fahrenheit);        //setup path and send readings
-  Firebase.pushString("/Healthcare/Heartbeat", myBPM);   
-  Firebase.pushString("/Healthcare/Gasvalue", sensorValue);  
-  Firebase.pushString("/Healthcare/Flexrange1", flexValue1);    
-  Firebase.pushString("/Healthcare/Flexrange2", flexValue2);    
-  Firebase.pushString("/Healthcare/Flexrange3", flexValue3);    
-  Firebase.pushString("/Healthcare/Flexrange4", flexValue4);    
-  Firebase.pushString("/Healthcare/vibration", value); 
-   
-}
-#include <ESP8266WiFi.h>    //esp8266 library
-#include <FirebaseArduino.h>     //firebase library
-#include <OneWire.h>
-#include <DallasTemperature.h>
-#include <PulseSensorPlayground.h>     // Includes the PulseSensorPlayground Library
-#define FIREBASE_HOST "healthcare-7c153.firebaseio.com"  // the project name address from firebase id
-#define FIREBASE_AUTH "qxAJyMdw8DdKt2RIvaBZdlyRBa7GcwbX1pfiv6Cm"  // the secret key generated from firebase
-#define WIFI_SSID "XXXXXXXXXXXX"                  // wifi name 
-#define WIFI_PASSWORD "XXXXXXXXX"                 //password of wifi 
-const int PulseWire = 0;  
-float temp;
-float Fahrenheit=0;
-int Threshold = 550;    
-int sensorvalue=A0;
-const int flexPin = A1; 
-const int flexPin = A2; 
-const int flexPin = A3 
-const int flexPin = A4; 
-int motorPin = 3;
-int value=0;
-motorpin==value;
-                                                    
-void setup() {
-  Serial.begin(9600);
-  delay(1000);
-  pinMode(motorPin, OUTPUT );
-  pulseSensor.analogInput(PulseWire);
-  pulseSensor.setThreshold(Threshold);   
-  DS18B20.begin();                
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);    
-  Serial.print("Connecting to ");
-  Serial.print(WIFI_SSID);
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(500);
-  }
-  Serial.println();
-  Serial.print("Connected to ");
-  Serial.println(WIFI_SSID);
-  Serial.print("IP Address is : ");
-  Serial.println(WiFi.localIP());                  //print local IP address
-  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);     // connect to firebase
-  if (pulseSensor.begin()) {
-    Serial.println("PulseSensor object created!");
-  }                         //Start reading dht sensor
-}
-void gas_level()
-{
-   int sensorValue = analogRead(A0);
-  // print out the value you read:
-  Serial.println(sensorValue);
-  delay(200);
-}
-void flex_sensor()
-{
-  int flexValue1;
-  int flexValue2;
-  int flexValue3;
-  int flexValue4;
-  flexValue = analogRead(flexPin);
-  flexValue = analogRead(flexPin);
-  flexValue = analogRead(flexPin);
-  flexValue = analogRead(flexPin);
-  Serial.print("sensor: ");
-  Serial.println(flexValue1);
-  Serial.print("sensor: ");
-  Serial.println(flexValue2);
-  Serial.print("sensor: ");
-  Serial.println(flexValue3);
-  Serial.print("sensor: ");
-  Serial.println(flexValue4);
-  }
-void loop() { 
-if(motorPin==HIGH)
-{
-    digitalWrite(motorPin, HIGH);
-    Serial.println("vibration detected");
-    Serial.println(value);
-    delay(1000);
-}
-else{
-    digitalWrite(motorPin, LOW);
-    Serial.println("vibration not detected");
-    Serial.println(value);
-    delay(1000);
-}
-  DS18B20.requestTemperatures(); 
-  temp = DS18B20.getTempCByIndex(0); // Celcius
-  Fahrenheit = DS18B20.toFahrenheit(temp); // Fahrenheit
-  Serial.println(temp);
-  Serial.println(Fahrenheit);
-  delay(4000);
-int myBPM = pulseSensor.getBeatsPerMinute();      // Calculates BPM
-
-  if (pulseSensor.sawStartOfBeat()) {               // Constantly test to see if a beat happened
-    Serial.println("♥  A HeartBeat Happened ! "); // If true, print a message
-    Serial.print("BPM: ");
-    Serial.println(myBPM);                        // Print the BPM value
-    }
-delay(20);
-gas_level()
-flex_sensor()
-  Firebase.pushString("/Healthcare/Temperature", temp);         //setup path and send readings
-  Firebase.pushString("/Healthcare/Temperature", Fahrenheit);        //setup path and send readings
-  Firebase.pushString("/Healthcare/Heartbeat", myBPM);   
-  Firebase.pushString("/Healthcare/Gasvalue", sensorValue);  
-  Firebase.pushString("/Healthcare/Flexrange1", flexValue1);    
-  Firebase.pushString("/Healthcare/Flexrange2", flexValue2);    
-  Firebase.pushString("/Healthcare/Flexrange3", flexValue3);    
-  Firebase.pushString("/Healthcare/Flexrange4", flexValue4);    
-  Firebase.pushString("/Healthcare/vibration", value); 
-   
+  Firebase.pushString("/Healthcare/Temperature", t);      //setup path and send readings
+  Firebase.pushString("/Healthcare/Temperature", Fah);      //setup path and send readings
+  Firebase.pushString("/Healthcare/Heartbeat", myBPM);
+  Firebase.pushString("/Healthcare/Gasvalue", sensorValue);
+  Firebase.pushString("/Healthcare/Flexrange1", flexValue);
+  Firebase.pushString("/Healthcare/vibration", vib_data);
+  delay(15000);
 }
